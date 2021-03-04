82083a3b3bd70aabba438a22ff24f2ecd18d1892
Michał Tatar <49119927+michaltatar@users.noreply.github.com>
2021-03-04 23:29:47 +0100

    Add files via upload

---
+++ src/components/Preview.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="preview-box"
+    style="width: 50vw; height: auto;"
+  >
+    {{ content ? content : 'Select a task from the left pane to preview the details' }}
+  </div>
+</template>
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: 'Preview',
+  computed: {
+    ...mapState(['items'])
+  },
+  data() {
+    return {
+      content: null
+    }
+  },
+  watch: {
+    items() {
+      this.content = this.items.find(i => i.selected)?.details
+    }
+  }
+}
+</script>
+<style>
+.preview-box {
+  padding: 1em .5em;
+  border: 1px solid #fff;
+}
+</style>
